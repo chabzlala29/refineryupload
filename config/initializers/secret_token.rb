02e83c95ca4c5e3ad10384da63e61041c03799c8
Charles Darwin Pobre <chabzpobre@Charless-MacBook-Air.local>
2014-01-28 15:29:34 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refinerycmstry::Application.config.secret_token = 'd19c1df02eadc78e0ce349ebbf36e860d24debc28971e57fed3a6510cb79100b9fdd254344d36ee1936b120828ba09eff0038b33f1bd7dd6358c3cf9772b36d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Refinerycmstry::Application.config.session_store :cookie_store, key: '_refinerycmstry_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Refinerycmstry::Application.config.session_store :active_record_store
